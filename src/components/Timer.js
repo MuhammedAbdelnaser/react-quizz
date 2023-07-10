fd51c3b9da64eca0468ceeae937ca30beb40dd9c
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-07-10 20:14:31 +0300

    create timer

---
+++ src/components/Timer.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+
+function Timer({ dispatch, secondsRemaining }) {
+    const mins = Math.floor(secondsRemaining / 60);
+    const secs = secondsRemaining % 60;
+    useEffect(function () {
+        const id = setInterval(() => {
+            dispatch({ type: 'tick' })
+        }, 1000);
+        // Clear interval on unmount to prevent memory leaks.
+        return () => clearInterval(id);
+    }, [dispatch])
+
+    return (
+        <div className='timer'>{mins < 10 && "0"}{mins}:{secs < 10 && "0"}{secs}</div>
+    )
+}
+
+export default Timer
