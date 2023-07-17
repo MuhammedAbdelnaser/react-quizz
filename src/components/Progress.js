934fd475dd995d93e58b98218ab647a8ddc4c588
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-07-17 15:21:56 +0300

    add context

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,5 +1,7 @@
+import { useQuiz } from "../contexts/QuizContext"
 
-function Progress({ index, numQuestions, points, maxPossiblePoints, answer }) {
+function Progress() {
+    const { index, numQuestions, points, maxPossiblePoints, answer } = useQuiz();
     return (
         <header className="progress">
             <progress max={numQuestions} value={index + Number(answer !== null)} />
